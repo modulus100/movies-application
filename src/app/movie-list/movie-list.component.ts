227aeb243ba5e0349e6503cd2fbcdfd9e6383de8
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-07-19 00:54:07 +0300

    loading bar

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -3,20 +3,25 @@ import {ActivatedRoute} from "@angular/router";
 import {MovieSearchResponse} from "../movie-service/models/movie-search-response.model";
 
 @Component({
-  selector: 'movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+    selector: 'movie-list',
+    templateUrl: './movie-list.component.html',
+    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+    public isLoading: boolean = false;
 
-  constructor(private route: ActivatedRoute) {
-  }
+    constructor(private route: ActivatedRoute) {
+    }
 
-  ngOnInit(): void {
-      this.route.data.subscribe( (response: MovieSearchResponse) => {
-          console.log(response)
-      });
-  }
+    ngOnInit(): void {
+        this.route.data.subscribe((response: MovieSearchResponse) => {
+            console.log(response)
+        });
+    }
+
+    search(event) {
+        this.isLoading = !this.isLoading;
+    }
 }
 
 
