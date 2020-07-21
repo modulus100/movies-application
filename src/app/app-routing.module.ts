4c23672ac5e777e744b7c250427c15a021dbb911
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-07-22 01:37:33 +0300

    better structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {MovieComponent} from "./app-modules/movie/movie.component";
-import {MovieListResolver} from "./app-modules/movie/resolvers/movie.resolver";
+import {MovieComponent} from "./modules/movie/movie.component";
+import {MovieListResolver} from "./modules/movie/resolvers/movie.resolver";
 
 
 const appRoutes: Routes = [
