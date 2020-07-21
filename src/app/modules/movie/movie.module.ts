056cbebb9c35a4d262e3dc136942895957418f43
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-07-22 02:02:55 +0300

    better structure

--- src/app/modules/movie/movie.module.ts
+++ src/app/modules/movie/movie.module.ts
@@ -1,16 +1,20 @@
 import {MovieComponent} from "./movie.component";
 import {SharedModule} from "../../shared/shared.module";
 import {NgModule} from "@angular/core";
-import {MovieStoreModule} from "./movie-store.module";
+import {MovieStoreModule} from "./movie-route-store/movie-store.module";
+import {MovieRouteModule} from "./movie-route-store/movie-route.module";
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 
 @NgModule({
     imports: [
         SharedModule,
-        MovieStoreModule
+        MovieStoreModule,
+        MovieRouteModule
     ],
     declarations: [
         MovieComponent,
+        MovieDetailsComponent,
     ]
 })
 export class MovieModule {}
