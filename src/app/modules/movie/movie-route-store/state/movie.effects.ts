00f0c29ba0010af87efe771d91d2d36a9e9949ec
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-07-23 14:15:00 +0300

    handling error step 2

--- src/app/modules/movie/movie-route-store/state/movie.effects.ts
+++ src/app/modules/movie/movie-route-store/state/movie.effects.ts
@@ -3,18 +3,20 @@ import {Actions, Effect, ofType} from "@ngrx/effects";
 import {MovieService} from "../../services/movie.service";
 import {catchError, map, mergeMap} from "rxjs/operators";
 import {Observable, of} from "rxjs";
-import {Action} from "@ngrx/store";
+import {Action, Store} from "@ngrx/store";
 import * as movieActions from "./movie.actions"
 import {MovieActionTypes} from "./movieActionTypes";
 import {MovieSearchResponse} from "../../models/movie-search-response.model";
 import {Movie} from "../../models/movie.model";
 import {LoadMovies} from "./movie.actions";
+import * as appStates from "./movie-state";
 
 @Injectable()
 export class MovieEffects {
 
     constructor(private actions: Actions,
-                private movieService: MovieService) {
+                private movieService: MovieService,
+                private store: Store<appStates.AppState>) {
     }
 
     initIds(movies: Array<Movie>): void {
@@ -28,13 +30,20 @@ export class MovieEffects {
         ofType<movieActions.LoadMovies>(MovieActionTypes.LOAD_MOVIES),
         mergeMap((action: LoadMovies) => this.movieService.searchMoviesContentByKeyword(action.searchKeyword)
             .pipe(
-                map((response: MovieSearchResponse) => {
-                        this.initIds(response.Search);
-                        return new movieActions.LoadMoviesSuccess(response.Search)
-                    }
-                ),
-                catchError(err => of(new movieActions.LoadMoviesFail(err)))
+                catchError(err => {
+                    this.store.dispatch(new movieActions.LoadMoviesFail(err))
+                    return of([])
+                })
             )
+        ),
+        map((response: MovieSearchResponse) => {
+                if (!response.Search) {
+                    return new movieActions.LoadMoviesSuccess([]);
+                }
+
+                this.initIds(response.Search);
+                return new movieActions.LoadMoviesSuccess(response.Search);
+            }
         )
     );
 }
