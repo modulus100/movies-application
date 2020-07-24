279058d4ce47645482010bdbc817c7aa460c7c2b
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-07-24 06:22:12 +0300

    redirect to main if not found

--- src/app/modules/movie/movie.component.ts
+++ src/app/modules/movie/movie.component.ts
@@ -1,14 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {MovieService} from "./services/movie.service";
 import {Movie} from "./models/movie.model";
 import {select, Store} from "@ngrx/store";
-import {combineLatest, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import * as appStates from "./movie-route-store/state/movie-state";
 import * as movieSelectors from "./movie-route-store/state/movie.selector";
 import * as movieActions from "./movie-route-store/state/movie.actions"
 import {AppState} from "./movie-route-store/state/movie-state";
-import {take} from "rxjs/operators";
 
 
 @Component({
@@ -20,8 +17,8 @@ export class MovieComponent implements OnInit {
     public loading: boolean = true;
     public searchText: String = "People";
     public noMoviesText: String = "There're no movies for this request, try something else";
-    public movies$: Observable<Array<Movie>>;
     public getMoviesError$: Observable<string>;
+    public movies$: Observable<Array<Movie>>;
 
     constructor(private store$: Store<appStates.AppState>) {}
 
